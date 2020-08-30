bbc1e892a169d2c40d0dadfc89908283e48365bc
LAM-NGUYEN\NTLNEO <lam.nguyenthanh84@gmail.com>
2020-08-30 08:56:58 +0700

    add file DEMO_FaceBooK

--- src/test/java/fiato/testing/Facebook.java
+++ src/test/java/fiato/testing/Facebook.java
@@ -23,7 +23,7 @@ import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.AndroidKeyCode;
+//import io.appium.java_client.android.AndroidKeyCode;
 import io.appium.java_client.android.nativekey.PressesKey;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
 
